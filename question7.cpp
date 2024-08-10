bfb8af713092f7fdbb212d335de13f9f92d01d6d
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-11 00:34:15 +0530

    Add files via upload

---
+++ question7.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int number;
+  printf("Enter Your Number:");
+  scanf("%d",&number);
+  if(number%5 ==0 || number%3 == 0 ){
+      printf("Divisible");
+  }
+  else{
+      printf("Not divisible");
+  }
+    return 0;
+}
