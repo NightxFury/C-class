58e870b865e4b3aa25e953a7a5f5979ceb00d831
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-12 23:48:06 +0530

    Add files via upload

---
+++ question11.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int sp;
+	int bp;
+	printf("enter sp:");
+	scanf("%d",&sp);
+	printf("enter bp:");
+	scanf("%d",&bp);
+	if(sp>bp){
+		printf("profit");
+	}
+	else{
+		if(sp = bp){
+			printf("No gain");
+		}
+		else{
+			printf("loss")
+		
+	return 0;
+}
+	
