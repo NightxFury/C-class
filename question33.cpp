d3eb8ff7ec5c52ae96a29e0238b8566a643fa3a0
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-09-09 01:53:07 +0530

    Add files via upload

---
+++ question33.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void england(){
+	printf("I am from england\n");
+	return ;
+}
+void australia(){
+	printf("You are from Australia\n");
+}
+void india(){
+	printf("He is an indian\n");
+	return;
+}
+int main(){
+	india();
+	england();
+	australia();
+	
+	return 0;
+}
