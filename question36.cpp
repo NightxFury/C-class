2539b8fc61fe33dc395eadea7cbe9ae76b9a77f0
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-09-09 01:53:54 +0530

    Add files via upload

---
+++ question36.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void fun(){
+	printf("hello");
+}
+int main(){
+	int n;
+	Printf("Enter Your number:");
+	sacanf("%d",&n);
+	for(int i; i <=n; i++){
+		printf(fun();)
+	}
+	return 0;
+}
